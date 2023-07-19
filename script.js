54ae3b5505e2c21ba6c9f1feb065f4c58a457762
Verseler <handuman.verselerkerr@gmail.com>
2023-07-19 22:45:45 +0800

    add: GUI

--- script.js
+++ script.js
@@ -2,41 +2,72 @@
 let GameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
   const boardBoxes = document.getElementsByClassName("gameboard__box");
+  const symbol = {
+    X: "assets/icon-x.svg",
+    O: "assets/icon-o.svg",
+    "x-gray": "assets/icon-x-gray.svg",
+    "o-gray": "assets/icon-o-gray.svg",
+  };
+
+  //when restart button is press restart the game
+  const restBtn = document.getElementById('restart-btn');
+  restBtn.addEventListener(('click'), () => {
+    console.log(1);
+    GameBoard.restartGame();
+  });
 
   return {
-    getBoard : () => {return board},
+    getBoard: () => {
+      return board;
+    },
 
-    getBoardBoxes : () => {return boardBoxes},
+    getBoardBoxes: () => {
+      return boardBoxes;
+    },
+
+    setNextTurnIcon: (iconName) => {
+      const turnIcon = document.getElementById("turn-icon");
+      turnIcon.src = symbol[iconName];
+    },
 
     //update the Gameboard displayed in the web
-    setDisplay : () => {
-        for (let i = 0; i < boardBoxes.length; i++) {
-        boardBoxes[i].innerHTML = board[i];
+    setDisplay: () => {
+      for (let i = 0; i < boardBoxes.length; i++) {
+        let key = board[i];
+        if (board[i] === "") {
+          continue;
+        } else {
+          boardBoxes[i].innerHTML = `<img src="${symbol[key]}" />`;
         }
+      }
     },
 
     //clear board
-    clearDisplay : () => {
-        for (let i = 0; i < boardBoxes.length; i++) {
-            boardBoxes[i].innerHTML = "";
-        }
+    clearDisplay: () => {
+      for (let i = 0; i < boardBoxes.length; i++) {
+        boardBoxes[i].innerHTML = ""; //clear gameboard display in web
+        board[i] = ""; //clear board array
+      }
+    },
+
+    restartGame: () => {
+      GameBoard.clearDisplay();
+      //!!!!!!add also, clear scores
     },
 
     //update gamboardArray based on the player move
-    setBoard : (movePosition, symbol) => {
-        board[movePosition] = symbol;
-        //after the move update the displayed gameboard
-        GameBoard.setDisplay(); 
+    setBoard: (movePosition, symbol) => {
+      board[movePosition] = symbol;
+      //after the move update the displayed gameboard
+      GameBoard.setDisplay();
     },
 
     //verify if player move is valid in gameboard
-    moveIsValid : (movePosition) => {
-        return board[movePosition] === "" ? true : false;
-    }
+    moveIsValid: (movePosition) => {
+      return board[movePosition] === "" ? true : false;
+    },
   };
 })();
-
-
 
 /* players object using factory function */
 let Players = (symbol) => {
@@ -44,11 +75,10 @@ let Players = (symbol) => {
 
   const makeMove = (move) => {
     GameBoard.setBoard(move, playerSymbol); //update current player move
-    isWin(); //check if current player win the game
   };
 
   //verify if win
-  const isWin = () => {
+  const isWin = (round) => {
     let board = GameBoard.getBoard();
     const winningCombination = [
       [0, 1, 2],
@@ -60,24 +90,36 @@ let Players = (symbol) => {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     //check if player moves is a winning combination
     winningCombination.forEach((combinations) => {
       if (
         combinations.every((combination) => board[combination] === playerSymbol)
       ) {
+        changeBoxBackground(combinations);
         alert(`${playerSymbol} Win the game`);
       }
     });
+
+    //if round is 9 end the game as a TIE
+    if (round === 9) {
+      alert(` TIE `);
+    }
   };
 
-  return { makeMove };
+  //change the background of box if it is a winning combination
+  const changeBoxBackground = (combinations) => {
+    let boardBoxes = GameBoard.getBoardBoxes();
+
+    combinations.forEach((index) => {
+      boardBoxes[index].style.backgroundColor = "#3b5968";
+    });
+  };
+
+  return { makeMove, isWin };
 };
 
-
-
-
 function gameStart() {
-  GameBoard.clearDisplay();
   const player1 = Players("X");
   const player2 = Players("O");
   let boardBoxes = GameBoard.getBoardBoxes();
@@ -90,9 +132,13 @@ function gameStart() {
       //verify first if move is valid before making a move
       if (GameBoard.moveIsValid(move)) {
         if (round % 2 === 0) {
-          player1.makeMove(move);
-        } else {
+          GameBoard.setNextTurnIcon("x-gray");
           player2.makeMove(move);
+          player2.isWin(round);
+        } else {
+          GameBoard.setNextTurnIcon("o-gray");
+          player1.makeMove(move);
+          player1.isWin(round);
         }
         round++;
       }
@@ -100,7 +146,6 @@ function gameStart() {
   }
 }
 
-
 window.onload = () => {
-    gameStart();
-}
+  gameStart();
+};
